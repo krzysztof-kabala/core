b09b1d9b1c30b0f8ddc2e70882498338e0e901d9
Krzysztof Kabala <krzysztof.kabala@amsterdam-standard.pl>
2016-04-07 11:19:12 +0200

    Update I18nTask.php

--- src/Task/I18nTask.php
+++ src/Task/I18nTask.php
@@ -8,7 +8,7 @@ use Vegas\Cli\TaskAbstract;
 
 /**
  * `php cli/cli.php vegas:i18n generate`
- * 
+ *
  * Task will generate "{langDir}/{lang}/LC_MESSAGES/messages.po" file for each `$langs` language item.
  * If final file exists, overwrite it by adding new translations, but without modify translated items.
  *
@@ -19,29 +19,29 @@ use Vegas\Cli\TaskAbstract;
  * Supported file types:
  * - PHP
  * - VOLT
- * 
+ *
  * HOWTO:
  * 1) Extend I18nTask into Your app
  * 2) Overwrite `$directories`, `$langDir` and `$langs` (+ other params if needed)
  * 3) Overwrite `addToFinalPo(PO_FILE_PATH);` to add database translation into final *.PO
- *  You may use `generatePoContent` method to generate PO content 
+ *  You may use `generatePoContent` method to generate PO content
  *  from array of string or array [msgid,?msgstr,?comment] where "?" keys are optional (see sample below)
  * 4) Use "POEdit" to manage translations
  *
  *
  * Sample of `addToFinalPo` using:
-```
-protected function addToFinalPo($tmpPo)
-{
-$tmpPo = $this->getTmpFile();
-
-file_put_contents($tmpPo, $this->generatePoContent([
-    'message1', 'other message', 'dynamic %s message'
-]));
-
-$this->addPo($tmpPo, $tmpPo);
-}
-```
+ * ```
+ * protected function addToFinalPo($tmpPo)
+ * {
+ * $newPo = $this->getTmpFile();
+ *
+ * file_put_contents($newPo, $this->generatePoContent([
+ * 'message1', 'other message', 'dynamic %s message'
+ * ]));
+ *
+ * $this->addPo($tmpPo, $newPo);
+ * }
+ * ```
  *
  * If You are using non-utf8 standard charset, consider adding own header into PO files.
  */
@@ -66,7 +66,7 @@ class I18nTask extends TaskAbstract
     protected $templateKeywords = [
         'i18n._', '_'
     ];
-    
+
     private $parser = '{xgettext} --omit-header --no-wrap --language="{lang}" --from-code="{encoding}" -k"{keys}" -j -o"{out}" {in}';
     private $templateParser = '{xgettextTemplate} --force-po=false --language="{lang}" --from-code="{encoding}" --keyword="{keys}" --output="{out}" {in}';
     private $mergePoCmd = '{msgcat} --force-po --no-wrap --use-first {extra} {base} -o {base} 2>/dev/null';
@@ -91,11 +91,11 @@ class I18nTask extends TaskAbstract
 
         $textTmpPo = $this->getTmpFile();
         $templateTmpPo = $this->getTmpFile();
-        
+
         foreach ($this->directories as $directory) {
             $directoryIterator = new \RecursiveIteratorIterator(
                 new \RecursiveDirectoryIterator(
-                    $directory, 
+                    $directory,
                     \RecursiveDirectoryIterator::SKIP_DOTS
                 ),
                 \RecursiveIteratorIterator::SELF_FIRST,
@@ -113,7 +113,7 @@ class I18nTask extends TaskAbstract
                 } elseif ( $this->isFile($file, 'volt') ) {
                     $tmpPo = $this->getTmpFile();
                     $this->exec($this->getTemplateParser('Volt', $file, $tmpPo));
-                    
+
                     $lines = count( file( $tmpPo ));
                     if ($lines > 2) {
                         $this->removeTopLines($tmpPo, 2);
@@ -130,11 +130,11 @@ class I18nTask extends TaskAbstract
         }
 
         echo "Preparing final files...\n";
-        
+
         $this->addPo($textTmpPo, $templateTmpPo);
 
         $this->addToFinalPo($textTmpPo);
-        
+
         $langPos = $this->savePo($textTmpPo);
 
         echo "\nGenerated files:";
@@ -156,7 +156,7 @@ class I18nTask extends TaskAbstract
      * @param string[]|array $items string|[msgid,?msgstr,?comment]
      * @return string
      */
-    protected function generatePoContent(array $items) 
+    protected function generatePoContent(array $items)
     {
         $poText = '';
 
@@ -203,19 +203,19 @@ class I18nTask extends TaskAbstract
     {
         $files = [];
 
-            foreach ($this->langs as $lang) {
-                $poBaseFile = $this->langDir . "/$lang/LC_MESSAGES/messages.po";
+        foreach ($this->langs as $lang) {
+            $poBaseFile = $this->langDir . "/$lang/LC_MESSAGES/messages.po";
 
-                if (!is_file($poBaseFile)) {
-                    mkdir( dirname($poBaseFile), 0777, true );
-                    touch($poBaseFile);
-                }
-
-                $this->addPo($poBaseFile, $poFile);
-
-                $files[] = $poBaseFile;
+            if (!is_file($poBaseFile)) {
+                mkdir( dirname($poBaseFile), 0777, true );
+                touch($poBaseFile);
             }
-        
+
+            $this->addPo($poBaseFile, $poFile);
+
+            $files[] = $poBaseFile;
+        }
+
         return $files;
     }
 
@@ -318,7 +318,7 @@ class I18nTask extends TaskAbstract
     {
         return is_file($filename) && preg_match('/^.+\.'.$ext.'$/i', $filename);
     }
-    
+
     private function removeTopLines($filename, $lines=0)
     {
         if ($lines<1)
